18ffc4b127fe22c2d936e7c45dff4dbf7a31d347
Benjamin Summers <ben@benjaminsummers.net>
2015-03-09 20:53:57 +0000

    Minor cleanup.

--- src/Language/Haskell/Preprocess/Macros.hs
+++ src/Language/Haskell/Preprocess/Macros.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, UnicodeSyntax #-}
+{-# LANGUAGE TemplateHaskell, UnicodeSyntax, QuasiQuotes #-}
 
 module Language.Haskell.Preprocess.Macros(ghcMacros) where
 
@@ -8,24 +8,24 @@ import Data.Monoid
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BS8
 
-import Data.FileEmbed
+import Data.String.Here
 
 
-ghcAutoConf ∷ BS.ByteString
-ghcAutoConf = $(embedFile "ghcautoconf.h")
+ghcAutoConf ∷ String
+ghcAutoConf = [hereFile|ghcautoconf.h|]
 
-machDeps ∷ BS.ByteString
-machDeps = $(embedFile "MachDeps.h")
+machDeps ∷ String
+machDeps = [hereFile|MachDeps.h|]
 
 compilerMacros ∷ String
-compilerMacros = unlines $
-  [ "#define __GLASGOW_HASKELL__ 708"
-  , "#define x86_64_HOST_ARCH 1"
-  , "#define linux_HOST_OS 1"
-  , "#define FLT_RADIX 2"
-  , "#define HAVE_POLL 1"
-  , "#define INTEGER_GMP 1"
-  ]
+compilerMacros = [here|
+#define __GLASGOW_HASKELL__ 708
+#define x86_64_HOST_ARCH 1
+#define linux_HOST_OS 1
+#define FLT_RADIX 2
+#define HAVE_POLL 1
+#define INTEGER_GMP 1
+|]
 
 ghcMacros ∷ String
-ghcMacros = compilerMacros ++ BS8.unpack(ghcAutoConf <> machDeps)
+ghcMacros = compilerMacros ++ ghcAutoConf ++ machDeps
