1ba64f44f412618c5923148f598580b61f14a6c0
Benjamin Summers <ben@benjaminsummers.net>
2015-03-10 00:07:19 +0000

    Added a test case for handling CPP macros.

--- src/Language/Haskell/Preprocess/Macros.hs
+++ src/Language/Haskell/Preprocess/Macros.hs
@@ -22,6 +22,7 @@ compilerMacros = x++"\n" where x = [here|
 #define __GLASGOW_HASKELL__ 708
 #define x86_64_HOST_ARCH 1
 #define linux_HOST_OS 1
+#define mingw32_HOST_OS 1
 #define FLT_RADIX 2
 #define HAVE_POLL 1
 #define INTEGER_GMP 1
